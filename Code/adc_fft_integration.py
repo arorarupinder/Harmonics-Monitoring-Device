2025ee7217a55a49f8d9faf650d7b30c7e49d85a
arorarupinder <113944927+arorarupinder@users.noreply.github.com>
2023-03-09 19:01:28 -0600

    Update adc_fft_integration.py

--- Code/adc_fft_integration.py
+++ Code/adc_fft_integration.py
@@ -1,26 +1,51 @@
+#ADC_FFT
 from ulab import numpy as np
-import sys
-import machine 
 import utime
+from time import sleep
+from machine import Timer
+import sys
 
-#@micropython.native
-def read_adc():
-    # initialize the ADC
-    adc = machine.ADC(28)
+# Initialize the ADC and timer
+adc = machine.ADC(28)
+tim = Timer(-1)
 
-    # set the number of samples to be taken
-    adc_values = np.zeros(1024)
+# Initialize the counter as a global variable
+count = 0
+
+# Set the size of the array to store the ADC values
+ARRAY_SIZE = 1024
+adc_values = np.zeros(ARRAY_SIZE, dtype=np.uint16)
+
+#function decorator
+@micropython.native
+
+# Define a function to read the ADC value
+def read_adc(timer):
+    # Check if we've read 1024 values
+    global count
+    if count >= ARRAY_SIZE:
+        # Stop the timer and return
+        tim.deinit()
+        return
     
-    # Calculate the time between samples
-    sample_time = 1 / 1080
+    # Increment the counter
+    count += 1
+    
+    # Trigger the ADC conversion and read the value
+    value = adc.read_u16()
+    
+    # Store the value in the array
+    adc_values[count-1] = value    
+      
 
-    #read ADC and store them in an array
-    for i in range(1024):
-        adc_values[i] = adc.read_u16()
-        #1080 samples per second 
-        utime.sleep(sample_time)
-        
-    return adc_values
+# Set the timer to trigger the read_adc function every 1 second
+tim.init(freq=1200, mode=machine.Timer.PERIODIC, callback=read_adc)
+
+# Wait for the timer to finish
+while count < ARRAY_SIZE:
+    pass
+
+print(adc_values)
 
 def process_signal(adc_values):
     # create a square wave
@@ -29,13 +54,20 @@ def process_signal(adc_values):
     #normalize array
     z = np.zeros(len(adc_values))
 
-    #perform fft
-    a, b= np.fft.fft(y, z)
-
+    #perform fft     
+    a, b= np.fft.fft(y, z)    
+    
     #the the absolute value of the array        
-    f1 = abs(a) + abs(b)
-
+    f1 = abs(a) + abs(b)    
+      
     return f1
+
+def time():    
+    start_time = utime.ticks_us()
+    x = process_signal(adc_values)
+    end_time = utime.ticks_us() - start_time
+    print("FFT duration (us):", end_time)
+    return end_time
 
 def save_to_txt(f1, file_name):
     #print the array without truncating
@@ -50,23 +82,23 @@ def save_to_txt(f1, file_name):
 def read_txt(file_name):
     # Read the data from the text file
     with open(file_name, "r") as f:
-        data = f.read()
+        data1 = f.read()
 
     # Split the string into a list of strings
-    data = data.split()
+    data1 = data1.split()
 
     #Convert the strings to floats and print it 
-    data_list = []
-    for i in data:
-        data_list.append(float(i))
-    print(data_list)
+    data1_list = []
+    for i in data1:
+        data1_list.append(float(i))
+    #print(data1_list)
 
-    return data_list
+    return data1_list
 
 
-def save_to_csv(data_list, file_name):
+def save_to_csv(data1_list, file_name):
     # Convert the float array to a list of strings
-    data_str = [str(x) for x in data_list]
+    data_str = [str(x) for x in data1_list]
 
     # Join the list of strings with commas
     csv_str = ",".join(data_str)
@@ -85,8 +117,10 @@ def save_to_csv(data_list, file_name):
     # Close the file
     f.close()
 
-adc_values = read_adc()
+
 f1 = process_signal(adc_values)
+end_time = time()
 save_to_txt(f1, "fft.txt")
-data_list = read_txt("fft.txt")
-save_to_csv(data_list, "fft.csv")
+data1_list = read_txt("fft.txt")
+save_to_csv(data1_list, "fft.csv")
+
